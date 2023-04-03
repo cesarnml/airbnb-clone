c0fcb5efb09d2325b88e2668f7f62fa727f20423
Cesar Mejia <cmejia@gmail.com>
2023-04-04 01:49:39 +0700

    fix ci

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,5 +1,6 @@
 import { afterEach } from 'vitest'
 import { cleanup } from '@testing-library/react'
+import { test, expect } from '@playwright/test'
 
 // extends Vitest's expect method with methods from react-testing-library
 import '@testing-library/jest-dom'
