221aef5a9dc5792bea166d8825393bc72e51a197
Cesar Mejia <cmejia@gmail.com>
2023-04-03 16:47:29 +0700

    fix layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,11 +25,9 @@ export default async function Home({ searchParams }: Props) {
     <ClientOnly>
       <Container>
         <div className='grid grid-cols-1 gap-8 pt-24 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6'>
-          <div>
-            {listings.map((listing) => (
-              <ListingCard key={listing.id} data={listing} currentUser={currentUser} />
-            ))}
-          </div>
+          {listings.map((listing) => (
+            <ListingCard key={listing.id} data={listing} currentUser={currentUser} />
+          ))}
         </div>
       </Container>
     </ClientOnly>
